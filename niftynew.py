ffdfe8e966de8423bc9843d2d59caf7ac4e655b2
Darshan Jadav <darshanjadav97@gmail.com>
2017-02-21 11:12:54 +0530

    Update niftynew.py

--- niftynew.py
+++ niftynew.py
@@ -5,15 +5,22 @@ from bs4 import BeautifulSoup
 soup = BeautifulSoup(page.content, 'html.parser')
 name_box = soup.find('h1', attrs={'class': 'b_20'})
 name = name_box.get_text()
-price = soup.find(attrs={'class' : 'FL gr_35'})
-pricequote = price.text.strip()
+print name
+price = soup.find(attrs={'class' : 'FL r_35'})
+pricequote = price.text
+print pricequote
+points = soup.find(attrs={'class': 'FL r_20 PT10 MT3'})
+point = points.text
+print point
+percentages = soup.find(attrs={'class': 'FL r_15 PT10 MT3 PL5'})
+percentage = percentages.text
+print percentage
 import csv
 from datetime import datetime
 with open('index.csv', 'a') as csv_file:
 	writer = csv.writer(csv_file)
-	writer.writerow([name, pricequote, datetime.now()])
+	writer.writerow([name, pricequote, point, percentage, datetime.now()])
 
 
 
 
-	
